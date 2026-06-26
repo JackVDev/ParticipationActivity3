f63b1a06f001208ab01910186cced54910f108f6
Jack Verdin <jackverdin@icloud.com>
2026-06-26 15:14:46 -0400

    Imported random, Class Die: added roll_dice() method

--- dice_roll.py
+++ dice_roll.py
@@ -4,6 +4,7 @@
     A program created for the Participation Activity 3 in CSCC CSCI-1511
     06/26/2026
 """
+import random
 
 class Die:
     """A simple class for simulating dice rolls"""
@@ -11,3 +12,6 @@ class Die:
     def __init__(self, sides):
         """Initializing the sides attribute"""
         self.sides = sides
+    
+    def roll_dice(self):
+        print(random.randint(1, self.sides))
